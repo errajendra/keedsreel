e8cba1a272fb67f88b541059a321cc2ffff91db5
Aashish <aashish@wooshelf.com>
2023-04-10 17:12:58 +0530

    [REF] code.

--- talvido_app/api/api_auth.py
+++ talvido_app/api/api_auth.py
@@ -5,16 +5,14 @@ from .serializers import (
     TalvidoMobileLoginSerializer,
     TavlidoGoogleLoginSerializer,
     TavlidoFacebokLoginSerializer,
-    RegenerateAccessTokenSerializer
+    RegenerateAccessTokenSerializer,
 )
 
 
 """This API handle login with mobile otp"""
 
 class LoginMobileOTPAPIView(APIView):
-    
-    def post(self,request):
-
+    def post(self, request):
         """serialize the data"""
         mobile_login_serializer = TalvidoMobileLoginSerializer(data=request.data)
 
@@ -22,26 +20,24 @@ class LoginMobileOTPAPIView(APIView):
         if mobile_login_serializer.is_valid():
             mobile_login_serializer.save()
             response = {
-                "status_code" : status.HTTP_200_OK,
-                "message" : "success",
+                "status_code": status.HTTP_200_OK,
+                "message": "success",
             }
-            return Response(response,status=status.HTTP_200_OK)
-        
+            return Response(response, status=status.HTTP_200_OK)
+
         """return this response if validation failed"""
         response = {
-            "status_code" : status.HTTP_400_BAD_REQUEST,
-            "message" : "bad request",
-            "data" : mobile_login_serializer.errors
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": "bad request",
+            "data": mobile_login_serializer.errors,
         }
-        return Response(response,status=status.HTTP_400_BAD_REQUEST)
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 """This API handle login with google"""
 
 class LoginGoogleAPIView(APIView):
-
-    def post(self,request):
-        
+    def post(self, request):
         """serialize the data"""
         google_login_serializer = TavlidoGoogleLoginSerializer(data=request.data)
 
@@ -49,26 +45,24 @@ class LoginGoogleAPIView(APIView):
         if google_login_serializer.is_valid():
             google_login_serializer.save()
             response = {
-                "status_code" : status.HTTP_200_OK,
-                "message" : "success",
+                "status_code": status.HTTP_200_OK,
+                "message": "success",
             }
-            return Response(response,status=status.HTTP_200_OK)
-        
+            return Response(response, status=status.HTTP_200_OK)
+
         """return this response if validation failed"""
         response = {
-            "status_code" : status.HTTP_400_BAD_REQUEST,
-            "message" : "bad request",
-            "data" : google_login_serializer.errors
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": "bad request",
+            "data": google_login_serializer.errors,
         }
-        return Response(response,status=status.HTTP_400_BAD_REQUEST)
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 """This API handle login with facebook"""
 
 class LoginFacebookAPIView(APIView):
-
-    def post(self,request):
-        
+    def post(self, request):
         """serialize the data"""
         facebook_login_serializer = TavlidoFacebokLoginSerializer(data=request.data)
 
@@ -76,43 +70,46 @@ class LoginFacebookAPIView(APIView):
         if facebook_login_serializer.is_valid():
             facebook_login_serializer.save()
             response = {
-                "status_code" : status.HTTP_200_OK,
-                "message" : "success",
+                "status_code": status.HTTP_200_OK,
+                "message": "success",
             }
-            return Response(response,status=status.HTTP_200_OK)
-        
+            return Response(response, status=status.HTTP_200_OK)
+
         """return this response if validation failed"""
         response = {
-            "status_code" : status.HTTP_400_BAD_REQUEST,
-            "message" : "bad request",
-            "data" : facebook_login_serializer.errors
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": "bad request",
+            "data": facebook_login_serializer.errors,
         }
-        return Response(response,status=status.HTTP_400_BAD_REQUEST)
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 """This api generate the access token using refresh token"""
 
 class RegenerateAccessTokenAPIVIew(APIView):
-
-    def post(self,request):
-
+    def post(self, request):
         """serialize the data"""
-        regenerate_access_token_serialzier = RegenerateAccessTokenSerializer(data=request.data)
+        regenerate_access_token_serialzier = RegenerateAccessTokenSerializer(
+            data=request.data
+        )
 
         """validate the data"""
         if regenerate_access_token_serialzier.is_valid():
-
             """generate the new token on the base of refresh token"""
             token_data = regenerate_access_token_serialzier.get_access_token(
-                grant_type = regenerate_access_token_serialzier.validated_data.get("grant_type"),
-                refresh_token=regenerate_access_token_serialzier.validated_data.get("refresh_token")
+                grant_type=regenerate_access_token_serialzier.validated_data.get(
+                    "grant_type"
+                ),
+                refresh_token=regenerate_access_token_serialzier.validated_data.get(
+                    "refresh_token"
+                ),
             )
-            return Response(token_data.json(),status=token_data.status_code)
-        
+            return Response(token_data.json(), status=token_data.status_code)
+
         """return this response if validation failed"""
         response = {
-            "status_code" : status.HTTP_400_BAD_REQUEST,
-            "message" : "bad request",
-            "data" : regenerate_access_token_serialzier.errors
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": "bad request",
+            "data": regenerate_access_token_serialzier.errors,
         }
-        return Response(response,status=status.HTTP_400_BAD_REQUEST)
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
--- talvido_app/api/urls.py
+++ talvido_app/api/urls.py
@@ -1,15 +1,27 @@
 from django.urls import path
-from .import (
+from . import (
     LoginMobileOTPAPIView,
     LoginGoogleAPIView,
     LoginFacebookAPIView,
-    RegenerateAccessTokenAPIVIew
+    RegenerateAccessTokenAPIVIew,
 )
 
 
 urlpatterns = [
-    path("auth/login/mobile-otp/", LoginMobileOTPAPIView.as_view(), name="login-mobile-otp-api"),
+    path(
+        "auth/login/mobile-otp/",
+        LoginMobileOTPAPIView.as_view(),
+        name="login-mobile-otp-api",
+    ),
     path("auth/login/google/", LoginGoogleAPIView.as_view(), name="login-google-api"),
-    path("auth/login/facebook/", LoginFacebookAPIView.as_view(), name="login-facebook-api"),
-    path("auth/regenerate-access-token/",RegenerateAccessTokenAPIVIew.as_view(),name="regenerate-access-token-api"),
+    path(
+        "auth/login/facebook/",
+        LoginFacebookAPIView.as_view(),
+        name="login-facebook-api",
+    ),
+    path(
+        "auth/regenerate-access-token/",
+        RegenerateAccessTokenAPIVIew.as_view(),
+        name="regenerate-access-token-api",
+    ),
 ]
