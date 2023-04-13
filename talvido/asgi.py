f6e6941e1c7eb8dac92d263da693f01a4c5f24c6
Aashish <aashish@wooshelf.com>
2023-04-13 11:38:47 +0530

    [modify] ASGI WSGI files.

--- talvido/asgi.py
+++ talvido/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+from .settings.base import DEBUG
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talvido.settings')
+if DEBUG:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talvido.settings.development')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talvido.settings.production')
 
 application = get_asgi_application()
--- talvido/wsgi.py
+++ talvido/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from .settings.base import DEBUG
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talvido.settings')
+if DEBUG:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talvido.settings.development')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talvido.settings.production')
 
 application = get_wsgi_application()
