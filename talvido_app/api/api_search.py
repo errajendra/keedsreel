ebab47550ef1642b5e1224df05d4eeca3288fcce
Aashish <aashish@wooshelf.com>
2023-04-30 09:14:28 +0530

    [REF] code.

--- talvido_app/api/api_search.py
+++ talvido_app/api/api_search.py
@@ -1,32 +1,43 @@
 from rest_framework import status
 from rest_framework.response import Response
 from talvido_app.models import Talvidouser, Post
-from talvido_app.api.serializers.search_serializers import SearchByUsernameModelSerializer
+from talvido_app.api.serializers.search_serializers import (
+    SearchByUsernameModelSerializer,
+)
 from talvido_app.api.serializers.post_serializers import GetPostModelSerializer
 from rest_framework.views import APIView
 
+
+"""This API will search the user account by username and 
+    return the result if matched"""
 
 class SearchByUsernameAPIView(APIView):
     def get(self, request):
         search_username = request.query_params.get("username", " ")
         user = Talvidouser.objects.filter(username__icontains=search_username)
-        user_serializer = SearchByUsernameModelSerializer(user, many=True, context={"request":request})
+        user_serializer = SearchByUsernameModelSerializer(
+            user, many=True, context={"request": request}
+        )
         response = {
-            "status_code" : status.HTTP_200_OK,
-            "message" : "ok",
-            "data" : user_serializer.data
+            "status_code": status.HTTP_200_OK,
+            "message": "ok",
+            "data": user_serializer.data,
         }
         return Response(response, status=status.HTTP_200_OK)
 
+
+"""This API will search the post by its description"""
 
 class SearchPostAPIView(APIView):
     def get(self, request):
         post_query = request.query_params.get("post", " ")
         post = Post.objects.filter(description__icontains=post_query)
-        post_serializer = GetPostModelSerializer(post, many=True, context={"request":request})
+        post_serializer = GetPostModelSerializer(
+            post, many=True, context={"request": request}
+        )
         response = {
-            "status_code" : status.HTTP_200_OK,
-            "message" : "ok",
-            "data" : post_serializer.data
+            "status_code": status.HTTP_200_OK,
+            "message": "ok",
+            "data": post_serializer.data,
         }
         return Response(response, status=status.HTTP_200_OK)
