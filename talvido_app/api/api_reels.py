568bd84dc04b4ced1971d6fefcf29a3c475d2917
Aashish <aashish@wooshelf.com>
2023-04-30 08:56:33 +0530

    [REF] code.

--- talvido_app/api/api_reels.py
+++ talvido_app/api/api_reels.py
@@ -1,11 +1,16 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers.reels_serializer import GetReelModelSerializer, UploadUserReelsModelSerializer
+from .serializers.reels_serializer import (
+    GetReelModelSerializer,
+    UploadUserReelsModelSerializer,
+)
 from rest_framework.permissions import IsAuthenticated
 from talvido_app.firebase.authentication import FirebaseAuthentication
 from talvido_app.models import Talvidouser, Reel
-    
+
+
+"""This API will get the user all and particular reels"""
 
 class GetUserReelsAPIView(APIView):
     authentication_classes = [FirebaseAuthentication]
@@ -15,34 +20,36 @@ class GetUserReelsAPIView(APIView):
         if id is None:
             user = Talvidouser.objects.get(firebase_uid=request.user)
             user_reels = user.reel_user.all()
-            reels_serializer = GetReelModelSerializer(user_reels, many=True, context={"request":request})
+            reels_serializer = GetReelModelSerializer(
+                user_reels, many=True, context={"request": request}
+            )
             response = {
-                "status_code" : status.HTTP_200_OK,
-                "message" : "ok",
-                "data" : reels_serializer.data
+                "status_code": status.HTTP_200_OK,
+                "message": "ok",
+                "data": reels_serializer.data,
             }
         else:
             try:
                 reel = Reel.objects.get(id=id)
             except Reel.DoesNotExist:
                 response = {
-                    "status_code" : status.HTTP_400_BAD_REQUEST,
-                    "message" : "bad request",
-                    "data" : [
-                        {
-                            "reel_id" : "This reel_id is invalid"
-                        }
-                    ]
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": "bad request",
+                    "data": [{"reel_id": "This reel_id is invalid"}],
                 }
                 return Response(response, status=status.HTTP_400_BAD_REQUEST)
-            reels_serializer = GetReelModelSerializer(reel, context={"request":request})
+            reels_serializer = GetReelModelSerializer(
+                reel, context={"request": request}
+            )
             response = {
-                "status_code" : status.HTTP_200_OK,
-                "message" : "ok",
-                "data" : reels_serializer.data
+                "status_code": status.HTTP_200_OK,
+                "message": "ok",
+                "data": reels_serializer.data,
             }
         return Response(response, status=status.HTTP_200_OK)
 
+
+"""This API will upload new reel for current authenticate user"""
 
 class UploadUserReelsAPIView(APIView):
     authentication_classes = [FirebaseAuthentication]
@@ -53,18 +60,20 @@ class UploadUserReelsAPIView(APIView):
         if upload_reels_serializer.is_valid():
             upload_reels_serializer.save(user=request.user)
             response = {
-                "status_code" : status.HTTP_201_CREATED,
-                "message" : "reel uploaded",
+                "status_code": status.HTTP_201_CREATED,
+                "message": "reel uploaded",
             }
             return Response(response, status=status.HTTP_201_CREATED)
 
         response = {
-            "status_code" : status.HTTP_400_BAD_REQUEST,
-            "message" : "bad request",
-            "data" : upload_reels_serializer.errors
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": "bad request",
+            "data": upload_reels_serializer.errors,
         }
         return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
+
+"""This API will get all user reels"""
 
 class GetUsersAllReelsAPIView(APIView):
     authentication_classes = [FirebaseAuthentication]
@@ -72,14 +81,18 @@ class GetUsersAllReelsAPIView(APIView):
 
     def get(self, request):
         reels = Reel.objects.select_related()
-        all_reels_serializer = GetReelModelSerializer(reels, many=True, context={"request":request})
+        all_reels_serializer = GetReelModelSerializer(
+            reels, many=True, context={"request": request}
+        )
         response = {
-            "status_code" : status.HTTP_200_OK,
-            "message" : "ok",
-            "data" : all_reels_serializer.data
+            "status_code": status.HTTP_200_OK,
+            "message": "ok",
+            "data": all_reels_serializer.data,
         }
         return Response(response, status=status.HTTP_200_OK)
 
+
+"""This API will get all the trendings reels"""
 
 class GetTrendingReelsAPIView(APIView):
     authentication_classes = [FirebaseAuthentication]
@@ -87,10 +100,12 @@ class GetTrendingReelsAPIView(APIView):
 
     def get(self, request):
         reels = Reel.objects.select_related()[:2]
-        all_reels_serializer = GetReelModelSerializer(reels, many=True, context={"request":request})
+        all_reels_serializer = GetReelModelSerializer(
+            reels, many=True, context={"request": request}
+        )
         response = {
-            "status_code" : status.HTTP_200_OK,
-            "message" : "ok",
-            "data" : all_reels_serializer.data
+            "status_code": status.HTTP_200_OK,
+            "message": "ok",
+            "data": all_reels_serializer.data,
         }
         return Response(response, status=status.HTTP_200_OK)
