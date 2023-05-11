9d7f3398fc20f4ae18b99b9cd36d885cd00897ea
Aashish <aashish@wooshelf.com>
2023-05-11 14:53:44 +0530

    [FIXED] issues.

--- talvido_app/models.py
+++ talvido_app/models.py
@@ -460,49 +460,49 @@ class ReferralUser(BaseModel):
         return str(self.referral_user.first_name) + " " + str(self.referral_user.last_name)
 
 
-class PointSetting(BaseModel):
-    activity = models.CharField(
-        choices=[
-            ('Time Spends', 'Time Spends'),
-            ('Referral', 'Referral'),
-            ('Share', 'Share'),
-            ('Comments', 'Comments'),
-            ('Like', 'Like'),
-        ],
-        verbose_name="User Activity",
-        unique=True)
-    count = models.IntegerField(
-        verbose_name="Activity Perform Count")
-    points = models.FloatField(
-        verbose_name="Points",
-        help_text="""Point will be added on user points when user perform activity on
-        given Activity Perform Count."""
-        )
+# class PointSetting(BaseModel):
+#     activity = models.CharField(
+#         choices=[
+#             ('Time Spends', 'Time Spends'),
+#             ('Referral', 'Referral'),
+#             ('Share', 'Share'),
+#             ('Comments', 'Comments'),
+#             ('Like', 'Like'),
+#         ],
+#         verbose_name="User Activity",
+#         unique=True)
+#     count = models.IntegerField(
+#         verbose_name="Activity Perform Count")
+#     points = models.FloatField(
+#         verbose_name="Points",
+#         help_text="""Point will be added on user points when user perform activity on
+#         given Activity Perform Count."""
+#         )
     
-    def __str__(self) -> str:
-        return f"{self.activity} - {self.count} - {self.points}"
+#     def __str__(self) -> str:
+#         return f"{self.activity} - {self.count} - {self.points}"
     
 
-class Point(BaseModel):
-    user = models.ForeignKey(
-        Talvidouser,
-        verbose_name="User",
-        on_delete=models.CASCADE,
-        related_name="user_point",
-    )
-    activity = models.CharField(
-        choices=[
-            ('Time Spends', 'Time Spends'),
-            ('Referral', 'Referral'),
-            ('Share', 'Share'),
-            ('Comments', 'Comments'),
-            ('Like', 'Like'),
-        ],
-        verbose_name="User Activity"
-    )
-    points = models.FloatField(verbose_name="Points")
+# class Point(BaseModel):
+#     user = models.ForeignKey(
+#         Talvidouser,
+#         verbose_name="User",
+#         on_delete=models.CASCADE,
+#         related_name="user_point",
+#     )
+#     activity = models.CharField(
+#         choices=[
+#             ('Time Spends', 'Time Spends'),
+#             ('Referral', 'Referral'),
+#             ('Share', 'Share'),
+#             ('Comments', 'Comments'),
+#             ('Like', 'Like'),
+#         ],
+#         verbose_name="User Activity"
+#     )
+#     points = models.FloatField(verbose_name="Points")
     
-    def __str__(self) -> str:
-        return f"{self.activity} - {self.points}"
+#     def __str__(self) -> str:
+#         return f"{self.activity} - {self.points}"
     
     
