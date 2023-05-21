5e4de5507efbbc554f35496b73695e3d19907b52
Aashish <aashish@wooshelf.com>
2023-05-21 12:36:24 +0530

    [IMPROVE] code.

--- payment/helpers.py
+++ payment/helpers.py
@@ -1,11 +1,11 @@
 from payment.models import UserSubscription
+from datetime import datetime
 
 
 def check_user_subscription(request):
     subscription = UserSubscription.objects.filter(user=request.user).order_by(
             "-created_at"
         )
-    from datetime import datetime
 
     if (
         subscription.exists()
