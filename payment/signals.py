5cb0d60051547026fc2467714c8e82c98cee9ba1
Aashish <aashish@wooshelf.com>
2023-05-22 11:09:40 +0530

    [IMP] razorpay signature verfication.

--- payment/signals.py
+++ payment/signals.py
@@ -1,10 +1,17 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import Transaction, UserSubscription
+from payment.razorpay.main import RazorpayClient
 
 
 """this signal will create user subscription, when new transaction succeed"""
 @receiver(post_save, sender=Transaction)
 def create_user_subscription(sender, instance, created, **kwargs):
     if created and instance.status == "SUCCESS":
+        razorpay = RazorpayClient()
+        razorpay.verify_payment_signature(
+            razorpay_order_id = instance.order_id,
+            razorpay_payment_id = instance.payment_id,
+            razorpay_signature = instance.signature
+        )
         UserSubscription.objects.create(user=instance.user)
